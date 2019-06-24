16e84728719ccb979ec3a1b236863fa904f44280
Bui Kiem <buivantrung1105@gmail.com>
2019-06-24 23:55:31 +0700

    Modify Django admin to list our custom user model.

---
+++ app/core/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+
+
+@admin.register(models.User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
