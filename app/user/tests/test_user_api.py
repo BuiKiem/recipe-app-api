c7f04687922924a61b2758c6486598db28042c37
Bui Anh Huy <bahuy@tma.com.vn>
2019-06-27 12:00:46 +0700

    Improve tests and refact assertEqual.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -6,7 +6,7 @@ from rest_framework import status
 
 
 CREATE_USER_URL = reverse('user:create')
-TOKEN_USER = reverse('user:token')
+TOKEN_URL = reverse('user:token')
 
 
 def create_user(**kwargs):
@@ -28,7 +28,7 @@ class PublicUserApiTestCase(TestCase):
         }
 
         response = self.client.post(CREATE_USER_URL, payload)
-        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         user = get_user_model().objects.get(**response.data)
         self.assertTrue(user.check_password(payload['password']))
@@ -44,7 +44,7 @@ class PublicUserApiTestCase(TestCase):
 
         response = self.client.post(CREATE_USER_URL, payload)
 
-        self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short(self):
         """Password must be more than 5 characters."""
@@ -54,7 +54,7 @@ class PublicUserApiTestCase(TestCase):
         }
         response = self.client.post(CREATE_USER_URL, payload)
 
-        self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
         user_exists = get_user_model().objects.filter(email=payload['email']).exists()
 
@@ -68,9 +68,9 @@ class PublicUserApiTestCase(TestCase):
         }
         create_user(**payload)
 
-        response = self.client.post(TOKEN_USER, payload)
+        response = self.client.post(TOKEN_URL, payload)
 
-        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn('token', response.data)
 
     def test_create_token_invalid_credentials(self):
@@ -81,9 +81,9 @@ class PublicUserApiTestCase(TestCase):
             'password': 'wrong',
         }
 
-        response = self.client.post(TOKEN_USER, payload)
+        response = self.client.post(TOKEN_URL, payload)
 
-        self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertNotIn('token', response.data)
 
     def test_create_token_no_user(self):
@@ -92,19 +92,31 @@ class PublicUserApiTestCase(TestCase):
             'email': 'user@example.com',
             'password': 'password',
         }
+        response = self.client.post(TOKEN_URL, payload)
 
-        response = self.client.post(TOKEN_USER, payload)
-
-        self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertNotIn('token', response.data)
+        self.assertIn('non_field_errors', response.data)
 
-    def test_create_token_missing_field(self):
+    def test_create_token_missing_password_field(self):
         """Both email and password are required to create token."""
         payload = {
-            'email': 'user@example.com',
+            'email': 'user@example.com', 'password': '',
         }
+        response = self.client.post(TOKEN_URL, payload)
 
-        response = self.client.post(TOKEN_USER, payload)
-
-        self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertNotIn('token', response.data)
+        self.assertIn('password', response.data)
+
+    def test_create_token_missing_email_field(self):
+        """Both email and password are required to create token."""
+        payload = {
+            'email': '',
+            'password': 'password',
+        }
+        response = self.client.post(TOKEN_URL, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', response.data)
+        self.assertIn('email', response.data)
